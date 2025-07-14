8792f209b5a50aab111f2147535b3ce5d5aacd40
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-07-14 09:32:18 -0300

    `Add MatchInformation class and update Match constructor. Implement validation for match status changes.`

--- API/Domain/Entities/Match.cs
+++ API/Domain/Entities/Match.cs
@@ -2,32 +2,136 @@ namespace Domain.Entities;
 
 public class Match
 {
-    protected Match() { }
+    protected Match()
+    {
+    }
+
     public Match(User fighterOne, User fighterTwo, DateTime date)
     {
         Id = Guid.NewGuid();
-        FighterOne = fighterOne;
-        FighterTwo = fighterTwo;
+        FighterOneInformation = new MatchInformation(fighterOne);
+        FighterTwoInformation = new MatchInformation(fighterTwo);
         Date = date.ToUniversalTime();
     }
+
     public Guid Id { get; set; }
-    public User FighterOne { get; set; }
-    public Guid FighterOneId { get; set; }
-    public User FighterTwo { get; set; }
-    public Guid FighterTwoId { get; set; }
+
+    public MatchInformation FighterOneInformation { get; set; }
+
+    public MatchInformation FighterTwoInformation { get; set; }
     public DateTime Date { get; set; }
-    public int FighterOneScore { get; set; }
-    public int FighterTwoScore { get; set; }
-    
+
+
     private FightStatus _status;
-    
+
     public string Status
     {
         get => _status.ToString();
-        set => _status = Enum.Parse<FightStatus>(value);
+        set => _status = SetStatus(value) ;
+    }
+    
+
+    private FightStatus SetStatus(string status)
+    {
+        if (Enum.TryParse<FightStatus>(status, out var result))
+        {
+            if (!ValidateStatusUpdate(result, out var err))
+
+            {
+                throw new FightSchoolServiceException(err);
+            }
+
+            if (result == FightStatus.Finalizado)
+            {
+                UpdateWinner();
+            }
+
+            return result;
+        }
+        throw new FightSchoolServiceException("Status Invalido");
+    }
+
+    private bool ValidateStatusUpdate(FightStatus status, out string error)
+    {
+        var defaultStatus = Enum.Parse<FightStatus>(Status);
+        switch ( defaultStatus)
+        {
+            case FightStatus.Finalizado:
+            {
+                error = "Luta concluída, não pode ser atualizada";
+                return false;
+            }
+            case FightStatus.Cancelado:
+            {
+                error = "Luta cancelada, não pode ser atualizada";
+                return false;
+            }
+            default:
+                break;
+                    
+        }
+        if (status == FightStatus.Finalizado)
+        {
+            if (FighterOneInformation.VictoryBySubmission && FighterTwoInformation.VictoryBySubmission)
+            {
+                error = 
+                    "Ambos lutadores finalizados, mas apenas um pode ser finalizado";
+                return false;
+
+            }
+
+            if (!FighterOneInformation.VictoryBySubmission && !FighterTwoInformation.VictoryBySubmission &&
+                FighterOneInformation.FighterScore == FighterTwoInformation.FighterScore &&
+                FighterOneInformation.Punishments == FighterTwoInformation.Punishments)
+            {
+                error = "Luta empatada";
+                return false;
+
+            }
+
+        }
+        error = string.Empty;
+        return true;
+    }
+
+    private void UpdateWinner()
+    {
+        if (!FighterOneInformation.VictoryBySubmission ||
+            FighterOneInformation.FighterScore < FighterTwoInformation.FighterScore ||
+            (FighterOneInformation.FighterScore == FighterTwoInformation.FighterScore &&
+             FighterOneInformation.Punishments > FighterTwoInformation.FighterScore))
+        {
+            Winner = 2;
+        }
+        else
+        {
+            Winner = 1;
+        }
     }
 
     public bool WasProcessed { get; set; } = false;
+
+    public int Winner { get; private set; }
+}
+
+public class MatchInformation
+{
+    protected MatchInformation(){}
+    public MatchInformation(User fighter)
+    {
+        User = fighter;
+        UserId = fighter.Id;
+    }
+    public MatchInformation(Guid fighterId)
+    {
+        UserId = fighterId;
+    }
+
+    public User? User { get; set; }
+    public Guid UserId { get; set; }
+    public int FighterScore { get; set; } = 0;
+    public int Punishments { get; set; } = 0;
+    public bool VictoryBySubmission { get; set; }
 }
 
 public enum FightStatus
