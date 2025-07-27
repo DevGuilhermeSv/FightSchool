e1b0ffb6c0206b0641548388cd250cad74719597
Guilherme Silva <development.guilherme@gmail.com>
2025-07-27 01:05:39 -0300

    fix: update import paths to use lowercase for components

--- SPA/figthSchool/src/App.jsx
+++ SPA/figthSchool/src/App.jsx
@@ -9,8 +9,8 @@ import {
   useLocation,
 } from "react-router-dom";
 import NewFight from "./pages/NewFight";
-import NavbarFs from "./Components/NavbarFs"; // Importe o NavbarFs
-import WithNavBar from "./Components/layout/WithNavBar";
+import NavbarFs from "./components/NavbarFs"; // Importe o NavbarFs
+import WithNavBar from "./components/layout/WithNavBar";
 import RankingPage from "./pages/RankingPage"; // Importa a página de ranking
 
 function AppRoutes({ setLogged, logged }) {
